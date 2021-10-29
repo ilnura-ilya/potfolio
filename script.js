f36e3af003159cc139ca20667b848848e8396537
Ilnura <t.ilnura@gmail.com>
2021-10-28 19:47:10 -0600

    new design

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
+window.addEventListener('load',function(){
+	$(".loader").fadeOut(1000);
+	$(".container").fadeIn(1000);
+});
 
 const hamburger = document.querySelector(".hamburger");
-console.log(hamburger);
 const navMenu = document.querySelector(".nav-menu");
 const navBar = document.querySelector(".navbar");
-console.log(navBar);
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -22,25 +23,17 @@ function closeMenu() {
     hamburger.classList.remove(".active");
     navMenu.classList.remove(".active");
 }
-/*
-$(document).ready(function() {
-	$("#fullpage").fullpage({
-		anchors: ["firstPage", "secondPage", "thirdPage"],
-		menu: "#menu",
-		autoScrolling:false
-		// responsiveWidth: 750
-	});
-});*/
+
 
 // ScrollMagic: init controller
-var controller = new ScrollMagic.Controller();
+let controller = new ScrollMagic.Controller();
 
 // Tween - Section 2
-var scene = new ScrollMagic.Scene({
+let scene = new ScrollMagic.Scene({
 	triggerElement: "#aboutSection"
 })
 // animate color and top border in relation to scroll position
-.setTween("#bg img", {
+.setTween(".imageBG", {
 	maxWidth: "1000px",
 	top: "30%",
 	left: "80%",
@@ -49,142 +42,18 @@ var scene = new ScrollMagic.Scene({
 // .addIndicators({ name: "2 (duration: 100)" }) // add indicators (requires plugin)
 .addTo(controller);
 
-
-
-
-
-
 // Tween - Section 3
-var scene = new ScrollMagic.Scene({
-	triggerElement: "#section3",
+scene = new ScrollMagic.Scene({
+	triggerElement: "#projects3",
 	duration: 300
 })
 
+// animate color and top border in relation to scroll position
+.setTween(".imageBG", {
+	top: "20%"
+}) // the tween duration can be omitted and defaults to 1
+
+// Add indicators (required plugin)
+.addTo(controller);
 
 
- //Display project
-
-/*const btn = document.querySelector('.projectName');
-const image = document.querySelector('.image-project');
-btn.addEventListener('click', projectDisplay);
-
-function projectDisplay (){
-	document.querySelector('.project-one').style.display = "none";
-	image.style.display = "block";
-
-}*/
-
-const videoOne = document.querySelector(".videoOne");
-const overlayOne = document.querySelector(".overlayOne");
-const closeOverlayOne = document.querySelector(".closeOverlayOne");
-console.log(closeOverlayOne);
-
-videoOne.addEventListener('click', function () {
-	overlayOne.classList.toggle("active");
- });
-closeOverlayOne.addEventListener('click', overlayClose);
-function overlayClose(){
-	overlayOne.classList.remove("active");
-};
-
-
-
-const videoTwo = document.querySelector(".videoTwo");
-const overlayTwo = document.querySelector(".overlayTwo");
-const closeOverlayTwo = document.querySelector(".closeOverlayTwo");
-
-
-videoTwo.addEventListener('click', function () {
-	overlayTwo.classList.toggle("active");
- });
-
-closeOverlayTwo.addEventListener('click', overlayCloseTwo);
-function overlayCloseTwo(){
-	overlayTwo.classList.remove("active");
-};
-
-
-const videoThree = document.querySelector(".videoThree");
-const overlayThree = document.querySelector(".overlayThree");
-const closeOverlayThree = document.querySelector(".closeOverlayThree");
-
-
-videoThree.addEventListener('click', function () {
-	overlayThree.classList.toggle("active");
- });
-
-closeOverlayThree.addEventListener('click', overlayCloseThree);
-function overlayCloseThree(){
-	overlayThree.classList.remove("active");
-};
-
-
-const videoFour = document.querySelector(".videoFour");
-console.log(videoFour);
-const overlayFour = document.querySelector(".overlayFour");
-console.log(overlayFour);
-const closeOverlayFour = document.querySelector(".closeOverlayFour");
-console.log(closeOverlayFour);
-
-
-videoFour.addEventListener('click', function () {
-	overlayFour.classList.toggle("active");
- });
-
-closeOverlayFour.addEventListener('click', overlayCloseFour);
-function overlayCloseFour(){
-	overlayFour.classList.remove("active");
-};
-
-
-const videoFive = document.querySelector(".videoFive");
-console.log(videoFive);
-const overlayFive = document.querySelector(".overlayFive");
-console.log(overlayFive);
-const closeOverlayFive = document.querySelector(".closeOverlayFive");
-console.log(closeOverlayFive);
-
-
-videoFive.addEventListener('click', function () {
-	overlayFive.classList.toggle("active");
- });
-
-closeOverlayFive.addEventListener('click', overlayCloseFive);
-function overlayCloseFive(){
-	overlayFive.classList.remove("active");
-};
-
-const videoSix = document.querySelector(".videoSix");
-console.log(videoFive);
-const overlaySix = document.querySelector(".overlaySix");
-console.log(overlayFive);
-const closeOverlaySix = document.querySelector(".closeOverlaySix");
-console.log(closeOverlayFive);
-
-
-videoSix.addEventListener('click', function () {
-	overlaySix.classList.toggle("active");
- });
-
-closeOverlaySix.addEventListener('click', overlayCloseSix);
-function overlayCloseSix(){
-	overlaySix.classList.remove("active");
-};
-
-
- /*
- document.querySelector('html').classList.remove('no-js');
-      if (!window.Cypress) {
-        const scrollCounter = document.querySelector('.js-scroll-counter');
-
-        AOS.init({
-          mirror: true
-        });
-
-        document.addEventListener('aos:in', function(e) {
-          console.log('in!', e.detail);
-        });
-
-        window.addEventListener('scroll', function() {
-          scrollCounter.innerHTML = window.pageYOffset;
-        });*/
