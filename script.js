f6536caea02099104f03e69efe4ed0ac976c4c1d
Ilnura <t.ilnura@gmail.com>
2022-07-08 14:20:34 -0600

    new commit changes

--- script.js
+++ script.js
@@ -58,3 +58,53 @@ scene = new ScrollMagic.Scene({
 .addTo(controller);
 
 
+
+// Wrap every letter in a span
+let textWrapper = document.querySelector('.ml12');
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+let textWrapperTwo = document.querySelector('.ml13');
+
+textWrapperTwo.innerHTML = textWrapperTwo.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+let tl = anime.timeline({loop: true});
+let tl2 = anime.timeline({loop: true});
+
+tl
+  .add({
+    targets: '.ml12 .letter, .ml13 letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 60 * i
+  }).add({
+    targets: '.ml12 .letterr',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+  
+
+tl2
+.add({
+  targets: '.ml13 .letter',
+  translateX: [40,0],
+  translateZ: 0,
+  opacity: [0,1],
+  easing: "easeOutExpo",
+  duration: 1200,
+  delay: (el, i) => 500 + 60 * i
+}).add({
+  targets: '.ml13 .letterr',
+  translateX: [0,-30],
+  opacity: [1,0],
+  easing: "easeInExpo",
+  duration: 1100,
+  delay: (el, i) => 100 + 30 * i
+});
